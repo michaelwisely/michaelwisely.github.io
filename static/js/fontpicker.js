59e9aac1e41fe82a9ec2dd49e6d18d0d1cdecde3
Michael Wisely <michaelwisely@gmail.com>
2013-01-24 11:57:18 -0600

    Did a little cleaning up and fixed brand to load more smoothly

--- static/js/fontpicker.js
+++ static/js/fontpicker.js
@@ -1,17 +1,20 @@
-fonts = [['Sonsie One', 54],
-         ['Poiret One', 90],
-         ['Freckle Face', 90],
-         ['Keania One', 90],
-         ['Rye', 78],
-         ['Play', 90],
-         ['Miss Fajardose', 100],
-         ['Yanone Kaffeesatz', 72]
-        ];
+var fonts = [['Sonsie One', 54],
+             ['Poiret One', 80],
+             ['Freckle Face', 80],
+             ['Keania One', 80],
+             ['Rye', 68],
+             ['Play', 80],
+             ['Miss Fajardose', 90],
+             ['Yanone Kaffeesatz', 72]
+            ];
+
+var index = Math.floor(Math.random() * fonts.length);
+var font_name = fonts[index][0];
+var font_size = fonts[index][1];
+
+$('#brand').css('font-family', font_name).css('font-size', font_size);
 
 $(function() {
-  var index = Math.floor(Math.random() * fonts.length);
-
-  $('#brand').css('font-family', fonts[index][0]);
-  $('#brand').css('font-size', fonts[index][1]);
+  $('#brand').show();
 });
 
